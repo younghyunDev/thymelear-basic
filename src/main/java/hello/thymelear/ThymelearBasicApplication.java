4b3e1f66cb1ed45b7d913570b31a0f986458dde2
Leeyeonghyeon <dudgus1202@gmail.com>
2022-05-13 08:57:36 +0900

    Initial commit

---
+++ src/main/java/hello/thymelear/ThymelearBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymelear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelearBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelearBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymelear/ThymelearBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymelear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymelearBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
